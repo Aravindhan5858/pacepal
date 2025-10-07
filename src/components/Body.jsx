1d899e3a06273bf69e661ca072ba0050546d3019
Aravindhan5858 <aravindhanchidambaram70@gmail.com>
2025-10-07 11:11:46 +0530

    Update Body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,7 +9,7 @@ function Body() {
         fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
             .then((val) => val.json())
             .then((res) => {
-                setData(res.data.cards[2].card.card.gridElements.infoWithStyle.restaurants)
+                setData(res.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
             })
     
     return (
